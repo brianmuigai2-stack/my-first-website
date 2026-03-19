2850797a0fa06a6e23b61713d5bd17c0d18c4f88
brianmuigai2-stack <brian.muigai2@student.moringaschool.com>
2026-03-19 17:18:19 +0300

    updated picture path

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   '/manifest.json',
   '/Yobi.jpg',
   '/profile.jpg',
-  '/App.png',
+  'App.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js'
 ];
